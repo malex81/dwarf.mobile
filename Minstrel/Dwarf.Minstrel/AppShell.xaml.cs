69bba27d665e67632e0f834b8744136458d2a499
Maxim <malex81@yandex.ru>
2024-12-27 15:58:04 +0300

    Minstrel billet

---
+++ Minstrel/Dwarf.Minstrel/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Dwarf.Minstrel
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Minstrel/Dwarf.Minstrel/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Dwarf.Minstrel
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
