4bdf2ff6e1a2242ad69b13c9aa72832532684802
Maxim <malex81@yandex.ru>
2025-03-24 13:17:11 +0300

    update MediaBox

--- Minstrel/Dwarf.Minstrel/MediaEngine/MediaBox.cs
+++ Minstrel/Dwarf.Minstrel/MediaEngine/MediaBox.cs
@@ -1,24 +1,23 @@
 ﻿using CommunityToolkit.Maui.Core.Primitives;
 using CommunityToolkit.Maui.Views;
+using CommunityToolkit.Mvvm.ComponentModel;
 using Dwarf.Framework.SystemExtension;
 
 namespace Dwarf.Minstrel.MediaEngine;
 
-public record MediaState(string URL, MediaElementState CurrentSate, string ErrorMessage);
+public record MediaBaxState(string URL, MediaElementState CurrentSate, string? ErrorMessage = null);
 
-public class MediaBox
+public partial class MediaBox : ObservableObject
 {
 	private readonly IApplication app;
 	private readonly DisposableList mediaHandlers = [];
 	private MediaElement? mediaPlayer;
 
+	[ObservableProperty]
+	public partial MediaBaxState State { get; set; } = new("", MediaElementState.None);
+
 	public MediaBox(IApplication app)
 	{
-		//mediaPlayer = new()
-		//{
-		//	IsVisible = false,
-		//	ShouldAutoPlay = true
-		//};
 		this.app = app;
 	}
 
@@ -44,14 +43,17 @@ public class MediaBox
 
 	private void MediaPlayer_MediaOpened(object? sender, EventArgs e)
 	{
+		State = State with { CurrentSate = mediaPlayer!.CurrentState };
 	}
 
 	private void MediaPlayer_MediaFailed(object? sender, MediaFailedEventArgs e)
 	{
+		State = State with { CurrentSate = mediaPlayer!.CurrentState, ErrorMessage = e.ErrorMessage };
 	}
 
 	public void PlayURL(string trackURL)
 	{
+		State = new(trackURL, MediaElementState.None);
 		var mp = GetPlayer();
 		if (mp == null) return;
 		mp.Source = MediaSource.FromUri(trackURL);
