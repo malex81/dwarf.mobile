5584745c525450534c7525d310ff31cdf01430ab
Maxim <malex81@yandex.ru>
2025-04-05 15:33:32 +0300

    show version

--- Minstrel/Dwarf.Minstrel/ViewModels/AppInfoPageModel.cs
+++ Minstrel/Dwarf.Minstrel/ViewModels/AppInfoPageModel.cs
@@ -1,10 +1,24 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System.Reflection;
 
 namespace Dwarf.Minstrel.ViewModels;
 
+public record InfoItem(string Name, string Value);
+
 public partial class AppInfoPageModel : ObservableObject
 {
-	[RelayCommand]
-	void Back() { }
+	public InfoItem[] InfoList { get; }
+
+	public AppInfoPageModel()
+	{
+		InfoList = BuildInfoList().ToArray();
+	}
+
+	static IEnumerable<InfoItem> BuildInfoList()
+	{
+		//var assm = Assembly.GetExecutingAssembly();
+		yield return new(AppInfo.Current.Name, "@ Dwarf holl");
+		yield return new("Версия", AppInfo.Current.VersionString);
+	}
 }
