3a996e125b6c7dcb876d6588eb7c777f5cf80685
Maxim <malex81@yandex.ru>
2025-04-27 20:12:57 +0300

    misc

--- Common/Dwarf.Framework/SystemExtension/DisposableList.cs
+++ Common/Dwarf.Framework/SystemExtension/DisposableList.cs
@@ -1,18 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
+﻿namespace Dwarf.Framework.SystemExtension;
 
-namespace Dwarf.Framework.SystemExtension
+public partial class DisposableList : List<IDisposable>, IDisposable
 {
-	public class DisposableList : List<IDisposable>, IDisposable
-	{
-		public DisposableList() : base() { }
-		public DisposableList(IEnumerable<IDisposable> source) : base(source) { }
+	public DisposableList() : base() { }
+	public DisposableList(IEnumerable<IDisposable> source) : base(source) { }
 
-		public void Dispose()
-		{
-			this.DisposeAll();
-			Clear();
-			GC.SuppressFinalize(this);
-		}
+	public void Dispose()
+	{
+		this.DisposeAll();
+		Clear();
+		GC.SuppressFinalize(this);
 	}
 }
