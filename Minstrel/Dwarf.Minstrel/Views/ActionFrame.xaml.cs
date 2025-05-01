f0bf45bed93748855c0540c22d37ba17924d5fc3
Maxim <malex81@yandex.ru>
2025-05-01 21:52:47 +0300

    misc

--- Minstrel/Dwarf.Minstrel/Views/ActionFrame.xaml.cs
+++ Minstrel/Dwarf.Minstrel/Views/ActionFrame.xaml.cs
@@ -4,12 +4,21 @@ namespace Dwarf.Minstrel.Views;
 
 public partial class ActionFrame : ContentView
 {
+#pragma warning disable CS0169
 	//https://github.com/rrmanzano/maui-bindableproperty-generator
 	[AutoBindable]
-	private readonly string _title;
+	private readonly string? _title;
+	[AutoBindable]
+	private readonly string? _description;
+	[AutoBindable]
+	private readonly ImageSource? _icon;
+	[AutoBindable]
+	private readonly ImageSource? _secondaryIcon;
+#pragma warning restore CS0169
 
 	public ActionFrame()
 	{
 		InitializeComponent();
+		Title = "asd";
 	}
 }
